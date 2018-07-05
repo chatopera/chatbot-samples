f56496e4fa2285d6c6eaba4f077fdb77cab42df6
Hai Liang Wang <hailiang.hl.wang@gmail.com>
2018-07-05 16:31:03 +0800

    实现回答天气描述

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# conversation-sampleapp
-多轮对话示例程序
+# Chatopera 多轮对话示例程序
+
+本示例程序是使用[多轮对话设计器](https://www.chatopera.com/product/conversation-designer)设计导出的项目文件。
+该项目打包后可导入到[智能问答引擎](https://www.chatopera.com/product/conversation-engine)中，作为多轮对话应用的运行时环境。
+
+# 说明
+该示例程序实现一个能回答天气情况的聊天机器人。
+
+
+# 打包
+
+
+# 导入到智能问答引擎
+
+
+# LICENSE
+[Apache 2.0](./LICENSE)
+
+
+[![chatoper banner][co-banner-image]][co-url]
+
+[co-banner-image]: https://user-images.githubusercontent.com/3538629/42217321-3d5e44f6-7ef7-11e8-94e7-1574bfa1dbb8.png
+[co-url]: https://www.chatopera.com
---
+++ app/plugin.js
@@ -0,0 +1,40 @@
+/**
+ * Plugin
+ */
+
+var WForewast = function (apiKey) {
+    if (!apiKey) throw new Error('Invalid token, get it from http://www.heweather.com/my/service');
+    this.key = apiKey;
+}
+
+
+WForewast.prototype.getWeatherByCity = function (city) {
+    return new Promise((resolve, reject)=>{
+        let url = config["HEWEATHER_URL"] + "/weather?city=" + encodeURIComponent(city) + "&key=" + this.key
+        http
+            .get(url)
+            .then((res)=>{
+                resolve(res.data.HeWeather5[0].suggestion);
+            })
+            .catch(function (err) {
+                if (err) return reject(err);
+            });
+    })
+}
+
+const wf = new WForewast(config["HEWEATHER_KEY"]);
+
+
+exports.getWeatherByCity = function(city, cb){
+    debug("getWeatherByCity: %s", city);
+    wf.getWeatherByCity(city)
+        .then((suggestions)=>{
+            cb(null, {
+                text: suggestions["air"]["txt"]
+            })
+        }, (err)=>{
+            cb(null, {
+                text: `很抱歉，没有获得${city}的天气信息。`
+            })
+        })
+}
